e336fa6c2e1f18a55e5a79c2044ea3a57470676d
motiejusvin <motiejus.vincer@protonamil.com>
2021-03-08 07:44:33 +0200

    Initial commit
    Initial commmit,added bug fixes,tho nothing has changed

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO.Ports;
+using System.IO;
+namespace osillo
+{
+    class Program
+    {
+        static int x = 0,prevy = 0;
+        static SerialPort sp = new SerialPort();
+        static int[] vls = new int[10000];
+        static int count = 0;
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            Console.Clear();
+            string data = "";
+            sp.PortName = "/dev/ttyUSB0";
+            sp.BaudRate = 2000000;
+            try
+            {
+                sp.Open();
+            }
+            catch
+            {
+                
+            }
+            while (true)
+            {
+                Read();
+            }
+            sp.Close();
+        }
+
+        public static void Read()
+        {
+            string message;
+            try
+            {
+                message = sp.ReadLine();
+            }
+            catch (TimeoutException)
+            {
+                message = "1";
+            }
+
+            try
+            {
+                int conv = Convert.ToInt32(message) / Console.BufferHeight;
+                if (x > Console.BufferWidth)
+                {
+                    Console.Clear();
+                    x = 0;
+                }
+
+                if (conv > prevy)
+                {
+                    for (; prevy < -conv+Console.BufferHeight; prevy++)
+                    {
+                        Console.SetCursorPosition(x,prevy);
+                        Console.Write("*");
+                    }
+                }
+                else if(conv < prevy)
+                {
+                    for (; prevy > -conv+Console.BufferHeight; prevy--)
+                    {
+                        Console.SetCursorPosition(x,prevy);
+                        Console.Write("*");
+                    }
+                }
+		prevy = -conv+Console.BufferHeight;
+                Console.SetCursorPosition(x, (-conv+Console.BufferHeight));
+                Console.Write("*");
+                x++;
+            }
+            catch
+            {
+                
+            }
+
+        }
+    }
+}
