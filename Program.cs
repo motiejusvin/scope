1f13e9072c5ee987ebce2928a0a4fecd5cfe3a8f
motiejusvin <motiejus.vincer@protonamil.com>
2021-03-08 20:02:53 +0200

    Added double channels,bug fixes
    Added double channels,still need to add select for that setting
    bug fixes: I did something,don't remember what tho ;-;

--- Program.cs
+++ Program.cs
@@ -5,17 +5,31 @@ namespace osillo
 {
     class Program
     {
-        static int x = 0,prevy = 0;
+	static string[] mes = new string[5];
+	static int ch = 0; 
+	static int freemode = 2;
+	static ConsoleKey kf;
+	static bool wasth = false;
+        static bool ground = false;
+	static int treshold = 500;
+	static int yoff = 0;
+        static int x = 0,prevy = 0,prevy2 = 0;
         static SerialPort sp = new SerialPort();
         static int[] vls = new int[10000];
-        static int count = 0;
         static void Main(string[] args)
         {
+	    Console.WriteLine("freemode");
+	    freemode = Convert.ToInt32(Console.ReadLine());
             Console.CursorVisible = false;
             Console.Clear();
             string data = "";
+	    try{
             sp.PortName = "/dev/ttyUSB0";
-            sp.BaudRate = 2000000;
+	    }
+	    catch{
+		sp.PortName = "/dev/ttyUSB1";
+	    }
+	    sp.BaudRate = 115200;
             try
             {
                 sp.Open();
@@ -24,6 +38,7 @@ namespace osillo
             {
                 
             }
+	    //System.Threading.Thread.Sleep(1000);
             while (true)
             {
                 Read();
@@ -34,47 +49,105 @@ namespace osillo
         public static void Read()
         {
             string message;
-            try
+	    if(freemode == 1 && wasth == false){
+		    Console.ReadKey();
+	    }
+	    try
             {
                 message = sp.ReadLine();
-            }
+		mes = message.Split("-");
+	    }
+
             catch (TimeoutException)
             {
-                message = "1";
+                message = "11";
             }
-
-            try
+            
+	    try
             {
-                int conv = Convert.ToInt32(message) / Console.BufferHeight ;
-                if (x > Console.BufferWidth)
-                {
-		    Console.Clear();
-                    x = 0;
-               	
-		}
 		
-                if (-conv+Console.BufferHeight > prevy)
-                {
-                    for (; prevy <= -conv+Console.BufferHeight; prevy++)
-                    {
-                        Console.SetCursorPosition(x,prevy);
-                        Console.Write("\u2588");
-                    }
-                }
-		else if(-conv+Console.BufferHeight < prevy)
-                {
-                    for (; prevy > -conv+Console.BufferHeight; prevy--)
-                    {
-                        Console.SetCursorPosition(x,prevy);
+		    if(ground == false && Convert.ToInt32(mes[ch]) < 10){
+			    ground = true;
+			    Console.WriteLine("Ground on");
+		    }
+                int conv = Convert.ToInt32(mes[0]) / Console.BufferHeight * 2;
+		int conv2 = Convert.ToInt32(mes[1]) / Console.BufferHeight * 2 + (Console.BufferHeight / 2);
+                if(Convert.ToInt32(mes[ch]) >= treshold  || freemode == 2 || wasth == true || freemode == 1  )
+		{
+			//Console.WriteLine(Convert.ToInt32(message).ToString());
+			//return
+			if(freemode == 3 || freemode == 1){
+				wasth = true;
+			}
+
+			if (x > Console.BufferWidth)
+			{   //conv += Console.BufferHeight / 3;
+			    if(freemode == 3)
+			    {
+				System.Environment.Exit(0);	    
+			    	Console.SetCursorPosition(Console.BufferWidth,Console.BufferHeight);
+			    }
+				if(freemode == 1)
+			    {
+				
+				Console.ReadKey();
+				x = 0;
+			    }
+			    else if(freemode == 2){
+				x = 0;
+			    }
+			     
+			    //System.Threading.Thread.Sleep(100);
+			    //Console.Clear();
+			    wasth = false;
+			    message = "";
+			}
+			for(int y = 0; y < Console.BufferHeight;y++){
+				Console.SetCursorPosition(x,y);
+				Console.Write(' ');
+			}	
+			if (-conv+Console.BufferHeight > prevy)
+			{
+			    for (; prevy <= -conv+Console.BufferHeight; prevy++)
+			    {
+				Console.SetCursorPosition(x,prevy);
+				Console.Write("\u2588");
+			    }
+			}
+			else if(-conv+Console.BufferHeight < prevy)
+			{
+			    for (; prevy > -conv+Console.BufferHeight; prevy--)
+			    {
+				Console.SetCursorPosition(x,prevy);
+				Console.Write("\u2588");
+			    }
+			}
+			if (-conv2+Console.BufferHeight > prevy2)
+			{
+			    for (; prevy2 <= -conv2+Console.BufferHeight; prevy2++)
+			    {
+				Console.SetCursorPosition(x,prevy2);
+				Console.Write("\u2588");
+			    }
+			}
+			else if(-conv2+Console.BufferHeight < prevy2)
+			{
+			    for (; prevy2 > -conv2+Console.BufferHeight; prevy2--)
+			    {
+				Console.SetCursorPosition(x,prevy2);
+				Console.Write("\u2588");
+			    }
+			}
+			Console.SetCursorPosition(x,-conv2+Console.BufferHeight);
 			Console.Write("\u2588");
-                    }
-                }
-		//prevy = -conv+Console.BufferHeight;
-                Console.SetCursorPosition(x, (-conv+Console.BufferHeight));
-                Console.Write("\u2588");
-                x++;
-            }
-            catch
+			Console.SetCursorPosition(x, (-conv+Console.BufferHeight));
+			Console.Write("\u2588");
+			x++;
+			//System.Threading.Thread.Sleep(1);
+		    }
+	    }
+            
+	    catch
             {
                 
             }
